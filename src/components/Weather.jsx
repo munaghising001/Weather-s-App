1de04b22bf656cca6a29026a36289d987b72efeb
munaghising001 <ghisingmuna001@gmail.com>
2026-05-27 16:25:53 +0545

    commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../assets/search.png';
 
 const Weather = () => {
   return (
-    <div>
-      <h1>Weather</h1>
-      
+    <div className='weather'>
+      <div className='search-bar'>
+        <input type='text' placeholder='Search' />
+        <img src={search_icon} alt="search icon" />
+      </div>
     </div>
   )
 }
