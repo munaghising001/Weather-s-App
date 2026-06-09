5dcca321b8461b122ea902a4f6615989041072ef
munaghising001 <ghisingmuna001@gmail.com>
2026-06-09 07:42:30 +0545

    search bar

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import './Weather.css'
+import React, { useEffect, useState } from 'react';
+import './Weather.css';
+
 import search_icon from '../assets/search.png';
 import clear_icon from '../assets/clear.png';
 import cloud_icon from '../assets/cloud.png';
@@ -9,37 +10,108 @@ import snow_icon from '../assets/snow.png';
 import wind_icon from '../assets/wind.png';
 import humidity_icon from '../assets/humidity.png';
 
-
-
-
 const Weather = () => {
+
+  const [weatherData, setWeatherData] = useState(null);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  };
+
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}&units=metric`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert("City not found");
+        return;
+      }
+
+      console.log(data);
+
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    search("New York");
+  }, []);
+
   return (
     <div className='weather'>
       <div className='search-bar'>
         <input type='text' placeholder='Search' />
-        <img src={search_icon} alt="search icon" />
+        <img
+          src={search_icon}
+          alt="search icon"
+          onClick={() => search("Kathmandu")}
+        />
       </div>
-      <img src={clear_icon} alt="" className='weather-icon' />
-      <p className='temperature'> 16°c</p>
-      <p className='location'>London</p>
-      <div className="weather-data">
-        <div className="col">
-          <img src={humidity_icon} alt="" />
-          <div>
-            <p>91 %</p>
-            <span>Humidiity</span>
-          </div>
-        </div>
-         <div className="col">
-          <img src={wind_icon} alt="" />
-          <div>
-            <p>91 km/h</p>
-            <span>Humidiity</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
 
-export default Weather
+      {weatherData && (
+        <>
+          <img
+            src={weatherData.icon}
+            alt=""
+            className='weather-icon'
+          />
+
+          <p className='temperature'>
+            {weatherData.temperature}°C
+          </p>
+
+          <p className='location'>
+            {weatherData.location}
+          </p>
+
+          <div className="weather-data">
+            <div className="col">
+              <img src={humidity_icon} alt="" />
+              <div>
+                <p>{weatherData.humidity}%</p>
+                <span>Humidity</span>
+              </div>
+            </div>
+
+            <div className="col">
+              <img src={wind_icon} alt="" />
+              <div>
+                <p>{weatherData.windSpeed} km/h</p>
+                <span>Wind Speed</span>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
